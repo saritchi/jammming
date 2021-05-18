6d37b78234f8b94fafcfb85a7bbba6375f361572
saritchi <s.ritchie1@hotmail.com>
2021-05-18 15:26:59 -0700

    Added method updateplaylistname

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -30,6 +30,7 @@ class App extends React.Component {
     }
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
+    this.updatePlaylistName = this.updatePlaylistName.bind(this);
   }
 
   addTrack(track) {
@@ -38,7 +39,7 @@ class App extends React.Component {
     } else {
       this.setState(prevState => ({
         playlistTracks: [...prevState.playlistTracks, track]
-      }))
+      }));
     }
   }
 
@@ -46,6 +47,10 @@ class App extends React.Component {
     this.setState({
       playlistTracks: this.state.playlistTracks.filter(playlistTrack => playlistTrack.id !== track.id)
     });
+  }
+
+  updatePlaylistName(name) {
+    this.setState({playlistName: name})
   }
 
   render(){
@@ -56,7 +61,7 @@ class App extends React.Component {
           <SearchBar/>
           <div className="App-playlist">
             <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack}/>
-            <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} onRemove={this.removeTrack}/>
+            <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} onRemove={this.removeTrack} onNameChange={this.updatePlaylistName}/>
           </div>
         </div>
       </div>
